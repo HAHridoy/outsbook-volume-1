fd1985769b99d9dad6246559b3cda6fe4f2591e2
HAHridoy <hosainoolalam123@gmail.com>
2023-10-16 18:45:01 +0600

    first commit

---
+++ income.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int result = n*365;
+    printf("%d\n",result);
+    return 0;
+}
---
+++ income.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int result = n*365;
+    cout<<result<<endl;
+    return 0;
+}
